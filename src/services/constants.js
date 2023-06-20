98c958b87d9aeb03d4130b03f0eff1ddf4bd4764
dewal.chaturvedi <dewal.chaturvedi>
2023-06-20 19:51:18 +0530

    changes

--- src/services/constants.js
+++ src/services/constants.js
@@ -1,3 +1,4 @@
 // export const apiDomain = 'http://103.150.186.45:80';
 // export const apiDomain = 'https://kabirtransportllc.com';
-export const apiDomain = 'https://aditya04848.pythonanywhere.com';
+// export const apiDomain = 'https://aditya04848.pythonanywhere.com';
+export const apiDomain = 'https://django.kabirtransportllc.com';
