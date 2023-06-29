ceb856135b0ec4d3d6a084e71ebd61586015cac0
dewal.chaturvedi <dewal.chaturvedi>
2023-06-29 13:08:41 +0530

    changes

---
+++ src/components/Disabled/Disabled.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Disabled = () => {
+    return (
+        <div style={{display:'flex',flex:1,justifyContent:'center',flexDirection:'column',alignItems:'center'}}>
+            <img style={{width:200,height:200}} src="https://media.istockphoto.com/id/1426108972/vector/prohibition-sign-monogram-icon-element-no-sign-empty-red-crossed-out-circle-vector-icon.jpg?s=612x612&w=0&k=20&c=UPGAPW4wUnKhL2sprTZv3U0RC2BeIu4N790NiyagQHM=" />
+            <h1>Your Access has been restricted as a result of an outstanding payment.</h1>
+        </div>
+    )
+}
+
+export default Disabled;
